63fd4ce3d298823c4d7253e77902c0c3c917ccd0
namanhvtcmobile <namanhvtcmobbile@gmail.com>
2022-10-25 03:08:17 +0700

    ok

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('http://localhost:3000')
+    console.log('http://localhost:5000s')
 });
 
 
