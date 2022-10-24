5170efafc284ef482c6fa29a02ecc6337b2072c7
namanhvtcmobile <namanhvtcmobbile@gmail.com>
2022-10-25 04:25:05 +0700

    ok

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
+
 const express = require('express');
-const morgan = require('morgan');
-const {engine} = require('express-handlebars');
 const path = require('path');
+const logger = require('morgan');
+const { engine } = require('express-handlebars');
+
+
+
+
 const app = express();
 
+
+// view engine setup
+// Template engine
+app.engine('hbs', engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src/resources/views'));
+
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'src/public')));
 
 
-// HTTP logger
-app.use(morgan('dev'));
-
-//Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
-app.set('view engine','hbs');
-app.set('views', path.join(__dirname, 'src/resources/views'));
-
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home');
 })
+
+
+
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server is running http://localhost:3000");
 });
-
 
