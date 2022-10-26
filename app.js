2962c9c13ed639686543ed8623443d154e3fb79a
namanhvtcmobile <namanhvtcmobbile@gmail.com>
2022-10-27 00:30:13 +0700

    done

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const { engine } = require('express-handlebars');
-
-
-
-
 const app = express();
 
+const route = require('./src/routes/index');
 
 // view engine setup
 // Template engine
@@ -21,17 +18,10 @@ app.set('views', path.join(__dirname, 'src/resources/views'));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'src/public')));
 
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());   
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/search', (req, res) => {
-  console.log(req.query.q);
-  res.render('search');
-})
-
-
+route(app);
 
 
 const port = process.env.PORT || 3000;
